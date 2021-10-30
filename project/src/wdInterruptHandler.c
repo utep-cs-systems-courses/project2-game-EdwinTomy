3b3e43b3b9a69dc401bb2a10ad051618ee8ddd28
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-10-30 12:21:46 -0600

    Update wdInterruptHandler.c

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -13,7 +13,7 @@ __interrupt_vec(WDT_VECTOR) WDT(){      /* 250 interrupts/sec */
     state_advance(blink_count);
     if (++blink_count >= 1000) blink_count = 0;
   }else if(sw2_state_down == 1){    // Second switch activates buzzer
-    play_happyBirthday(blink_count);
+    play_harryPotter(blink_count);
     if(++blink_count >= 4000) blink_count = 0;
   }else if(sw3_state_down == 1){   // Third switch that activate assy code for leds
     alternate_leds(blink_count);
