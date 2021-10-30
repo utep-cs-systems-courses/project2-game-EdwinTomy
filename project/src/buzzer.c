8d69a1f17836b97560cad5244a1f081c60b826ca
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-10-30 13:11:47 -0600

    Update buzzer.c

--- project/src/buzzer.c
+++ project/src/buzzer.c
@@ -38,49 +38,49 @@ void play_harry_potter(int sec_cnt)
   if(sec_cnt <= 0){
     buzzer_set_period(0);
     
-  } else if(sec_cnt <= 500/4){
+  } else if(sec_cnt >= 10250/4 -  500/4){
     buzzer_set_period(617);
     
-  } else if(sec_cnt <= 1250/4){
+  } else if(sec_cnt >= 10250/4 - 1250/4){
     buzzer_set_period(824);
     
-  } else if(sec_cnt <= 1500/4){
+  } else if(sec_cnt >= 10250/4 - 1500/4){
     buzzer_set_period(980);
     
-  } else if(sec_cnt <= 2000/4){
+  } else if(sec_cnt >= 10250/4 - 2000/4){
     buzzer_set_period(873);
     
-  } else if(sec_cnt <= 3000/4){
+  } else if(sec_cnt >= 10250/4 - 3000/4){
     buzzer_set_period(824);
     
-  } else if(sec_cnt <= 3500/4){
+  } else if(sec_cnt >= 10250/4 - 3500/4){
     buzzer_set_period(1234);
     
-  } else if(sec_cnt <= 4750/4){
+  } else if(sec_cnt >= 10250/4 - 4750/4){
     buzzer_set_period(1100);
     
-  } else if(sec_cnt <= 6000/4){
+  } else if(sec_cnt >= 10250/4 - 6000/4){
     buzzer_set_period(925);
     
-  } else if(sec_cnt <= 4750/4){
+  } else if(sec_cnt >= 10250/4 - 4750/4){
     buzzer_set_period(1100);
     
-  } else if(sec_cnt <= 6750/4){
+  } else if(sec_cnt >= 10250/4 - 6750/4){
     buzzer_set_period(824);
     
-  } else if(sec_cnt <= 7000/4){
+  } else if(sec_cnt >= 10250/4 - 7000/4){
     buzzer_set_period(980);
     
-  } else if(sec_cnt <= 7500/4){
+  } else if(sec_cnt >= 10250/4 - 7500/4){
     buzzer_set_period(873);
     
-  } else if(sec_cnt <= 8500/4){
+  } else if(sec_cnt >= 10250/4 - 8500/4){
     buzzer_set_period(777);
     
-  } else if(sec_cnt <= 9000/4){
+  } else if(sec_cnt >= 10250/4 - 9000/4){
     buzzer_set_period(873);
     
-  } else if(sec_cnt <= 10250/4){
+  } else if(sec_cnt >= 10250/4 - 10250/4){
     buzzer_set_period(617);
  
   } else {
