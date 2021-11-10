1b867dfea3434d67b6c7fddcab4609532d0584ae
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-11-10 15:43:07 -0700

    Update wdInterruptHandler.c

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -10,15 +10,19 @@ __interrupt_vec(WDT_VECTOR) WDT(){      /* 250 interrupts/sec */
   blink_count++;
   if(sw1_down == 1){ //if sw1 pressed
     play_harry_potter();
-  }else if(sw2_down == 1 ){ //if sw2 pressed
+    
+  } else if(sw2_down == 1){ //if sw2 pressed
     play_harry_potter();
-  }else if(sw3_down == 1){  
+    
+  } else if(sw3_down == 1){  
     dimLights();
-  }else if(sw4_down ==1){
+    
+  } else if(sw4_down == 1){
     buzzer_off();
     led_off();
     buzzer_set_period(0);
-  }else{
+    
+  } else {
     buzzer_set_period(0);
   }
 }
