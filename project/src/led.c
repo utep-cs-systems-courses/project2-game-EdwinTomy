0ca8ee9d69b4f799d474ec778132e3ab1c540d2e
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-11-10 16:18:24 -0700

    Update led.c

--- project/src/led.c
+++ project/src/led.c
@@ -3,13 +3,13 @@
 
 void led_init()
 {
-  P2DIR |= LEDS;            // Set LEDS to be outputs 
-  P2OUT &= ~LED_GREEN;      // Turn off green LED
-  P2OUT |= LED_RED;         // Turn on red LED
+  P1DIR |= LEDS;            // Set LEDS to be outputs 
+  P1OUT &= ~LED_GREEN;      // Turn off green LED
+  P1OUT |= LED_RED;         // Turn on red LED
 }
 
 void led_off()
 {
-  P2OUT &= ~LED_GREEN;
-  P2OUT &= ~LED_RED;
+  P1OUT &= ~LED_GREEN;
+  P1OUT &= ~LED_RED;
 }
