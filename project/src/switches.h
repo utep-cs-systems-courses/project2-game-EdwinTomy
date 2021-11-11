1884f17d070567e44bfee7e152a3821ffebb0c0b
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-11-10 17:03:07 -0700

    Update switches.h

--- project/src/switches.h
+++ project/src/switches.h
@@ -1,11 +1,11 @@
 #ifndef switches_included
 #define switches_included
 
-#define SWS1 BIT0
-#define SWS2 BIT1
-#define SWS3 BIT2
-#define SWS4 BIT3
-#define SWITCHES (SWS1 | SWS2 | SWS3 | SWS4)
+#define SW1 BIT0
+#define SW2 BIT1
+#define SW3 BIT2
+#define SW4 BIT3
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)
 
 void switch_init();
 void switch_interrupt_handler();
