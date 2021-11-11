bcba8b477b98d3abc6f7a0664e378523b9c70332
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-11-10 17:36:34 -0700

    Update switches.c

--- project/src/switches.c
+++ project/src/switches.c
@@ -4,22 +4,20 @@
 #include "buzzer.h"
 #include "led.h"
 
-sw1Down = 0;
-sw2Down = 0;
-sw3Down = 0;
-sw4Down = 0;
+char sw1_down;
+char sw2_down;
+char sw3_down;
+char sw4_down;
 
 static char
 switch_update_interrupt_sense()
 {
   char p2val = P2IN;
-
   /* update switch interrupt to detect changes from current buttons */
   P2IES |= (p2val & SWITCHES);/* if switch up, sense down */
   P2IES &= (p2val | ~SWITCHES);/* if switch down, sense up */
   return p2val;
 }
-
 void
 switch_init()/* setup switch */
 {
@@ -29,40 +27,31 @@ switch_init()/* setup switch */
   P2DIR &= ~SWITCHES;/* set switches' bits for input */
   switch_update_interrupt_sense();
 }
-
+void turnOff(){
+  sw1_down &= ~sw1_down;
+  sw2_down &= ~sw2_down;
+  sw3_down &= ~sw3_down;
+  sw4_down &= ~sw4_down;
+}
 void
 switch_interrupt_handler()
 {
   char p2val = switch_update_interrupt_sense();
-  
-  int prev1 = sw1Down;
-  int prev2 = sw2Down;
-  int prev3 = sw3Down;
-  int prev4 = sw4Down;
 
-  // Checks if button has been pressed
-  sw1Down = (p2val & SW1) ? 0 : 1;
-  sw2Down = (p2val & SW2) ? 0 : 1;
-  sw3Down = (p2val & SW3) ? 0 : 1;
-  sw4Down = (p2val & SW4) ? 0 : 1;
-
-  // Press button once to turn off, press again to turn off
-  if (prev1 != sw1Down && sw1Down){
-    oddPress1 ^= 1;
-    // One button pressed, all other should be off
-    oddPress2 = 0, oddPress3 = 0, oddPress4 = 0;
-  }
-  else if (prev2 != sw2Down && sw2Down){
-    oddPress2 ^= 1;
-    oddPress1 = 0, oddPress3 = 0, oddPress4 = 0;
-  }
-  else if (prev3 != sw3Down && sw3Down){
-    oddPress3 ^= 1;
-    oddPress1 = 0, oddPress2 = 0, oddPress4 = 0;
-  }
-  else if (prev4 != sw4Down && sw4Down){
-    oddPress4 ^= 1;
-    oddPress1 = 0, oddPress2 = 0, oddPress3 = 0;
+  if((p2val & SWITCHES) == 14){ //toggle sw1 on or off
+    turnOff();
+    sw1_down ^= 1;
+  } else if((p2val & SWITCHES) == 13){ //toggle sw2 on or off
+    turnOff();
+    sw2_down ^= 1;
+  } else if((p2val & SWITCHES) == 11){
+    turnOff();
+    sw3_down ^= 1;
+  } else if((p2val & SWITCHES) == 7){
+    turnOff();
+    sw4_down ^= 1;
+  } else {
+    buzzer_set_period(0); //if no switches are active
   }
 }
 
