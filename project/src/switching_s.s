d07d9ae64a5753b9b590e2c24c00c830b5040bbe
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-11-10 22:30:57 -0700

    Create switching_s.s
    assembly

---
+++ project/src/switching_s.s
@@ -0,0 +1,24 @@
+  .arch msp430g2553
+	.p2align 1,0
+
+	.extern blink_count
+	.extern green_turn
+	.extern red_turn
+  
+
+switching:
+
+	cmp #63, &blink_count
+	jl less
+	jmp more
+	
+less:
+	xor #red_turn, &P1OUT
+  jmp out
+  
+more:
+	xor #green_turn, &P1OUT
+  jmp out
+  
+out:
+	pop r0
