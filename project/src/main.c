4732018808820eec4d9fbdff2cfa1444e90eae7b
EdwinTomy <57510528+EdwinTomy@users.noreply.github.com>
2021-11-12 10:21:36 -0700

    Update main.c

--- project/src/main.c
+++ project/src/main.c
@@ -130,5 +130,10 @@ __interrupt_vec(WDT_VECTOR) WDT(){      /* 250 interrupts/sec */
     buzzer_off();
     led_off();
     buzzer_set_period(0);
+    
+  } else{
+    buzzer_off();
+    led_off();
+    buzzer_set_period(0);
   } 
 }
